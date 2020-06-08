c1b6129f2529fa4df0abb47f6fd1f7b5bd5db757
Tyler Young <tyler.young@impromptu.ninja>
2020-06-08 10:08:42 -0400

    implements shared string dictionary and handshake from net-code-2

--- Robust.Server/Maps/MapLoader.cs
+++ Robust.Server/Maps/MapLoader.cs
@@ -879,6 +879,7 @@ namespace Robust.Server.Maps
 
                 RootNode = stream.Documents[0].RootNode;
                 GridCount = ((YamlSequenceNode)RootNode["grids"]).Children.Count;
+                RobustSerializer.MappedStringSerializer.AddStrings(stream);
             }
         }
     }
